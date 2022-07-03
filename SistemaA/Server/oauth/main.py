9878adf7d8d6d802736f5aaf486b7a2ab1811185
Ricardo Santos <xricardosantos@gmail.com>
2022-07-03 18:37:30 -0300

    .

--- SistemaA/Server/oauth/main.py
+++ SistemaA/Server/oauth/main.py
@@ -2,6 +2,7 @@ import jwt
 
 from fastapi import FastAPI, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from fastapi.middleware.cors import CORSMiddleware
 from passlib.hash import bcrypt
 from tortoise import fields 
 from tortoise.contrib.fastapi import register_tortoise
@@ -10,12 +11,26 @@ from tortoise.models import Model
 
 app = FastAPI()
 
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 JWT_SECRET = 'myjwtsecret'
 
 class User(Model):
     id = fields.IntField(pk=True)
     username = fields.CharField(50, unique=True)
     password_hash = fields.CharField(128)
+    active = fields.BooleanField(default=True)
 
     def verify_password(self, password):
         return bcrypt.verify(password, self.password_hash)
@@ -35,27 +50,36 @@ async def authenticate_user(username: str, password: str):
 
 @app.post('/token')
 async def generate_token(form_data: OAuth2PasswordRequestForm = Depends()):
-    user = await authenticate_user(form_data.username, form_data.password)
+    try:
+        print ('form_data', form_data.username)  
+        user = await authenticate_user(form_data.username, form_data.password)
 
-    if not user:
+        if not user:
+            print ('Invalid username or password')
+            raise HTTPException(
+                status_code=status.HTTP_204_NO_CONTENT, 
+                detail='Invalid username or password'
+            )
+
+        user_obj = await User_Pydantic.from_tortoise_orm(user)
+
+        token = jwt.encode(user_obj.dict(), JWT_SECRET)
+
+        return {'access_token' : token, 'token_type' : 'bearer'}
+    except Exception as e:
+        print ('Exception', e)
         raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED, 
-            detail='Invalid username or password'
+            status_code=status.HTTP_204_NO_CONTENT,
+            detail=str(e)
         )
 
-    user_obj = await User_Pydantic.from_tortoise_orm(user)
-
-    token = jwt.encode(user_obj.dict(), JWT_SECRET)
-
-    return {'access_token' : token, 'token_type' : 'bearer'}
-
-async def get_current_user(token: str = Depends(oauth2_scheme)):
+async def get_current_user(token: str = Depends(oauth2_scheme) ):
     try:
         payload = jwt.decode(token, JWT_SECRET, algorithms=['HS256'])
         user = await User.get(id=payload.get('id'))
     except:
         raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED, 
+            status_code=status.HTTP_204_NO_CONTENT, 
             detail='Invalid username or password'
         )
 
@@ -63,7 +87,7 @@ async def get_current_user(token: str = Depends(oauth2_scheme)):
 
 
 @app.post('/users', response_model=User_Pydantic)
-async def create_user(user: UserIn_Pydantic):
+async def create_user(user: UserIn_Pydantic):#, userAdm: User_Pydantic = Depends(get_current_user)):
     user_obj = User(username=user.username, password_hash=bcrypt.hash(user.password_hash))
     await user_obj.save()
     return await User_Pydantic.from_tortoise_orm(user_obj)
@@ -72,9 +96,25 @@ async def create_user(user: UserIn_Pydantic):
 async def get_user(user: User_Pydantic = Depends(get_current_user)):
     return user    
 
+DATABASE_URL = "mysql://{}:{}@{}:{}/{}".format(
+    "rick",
+    "ri$enh@1979",
+    "localhost",
+     "3306",
+    "dblogin"
+)
+
+
+#print( "Using pymysql:" )
+#import pymysql
+#myConnection = pymysql.connect( host="localhost" , user="rick", passwd="ri$enh@1979", db="dblogin" )
+#doQuery( myConnection )
+#myConnection.close()
+
+
 register_tortoise(
     app, 
-    db_url='sqlite://database.sqlite',
+    db_url=DATABASE_URL,
     modules={'models': ['main']},
     generate_schemas=True,
     add_exception_handlers=True
--- SistemaA/js/script.js
+++ SistemaA/js/script.js
@@ -1,16 +1,42 @@
 function CheckLogin() {
     var email = document.getElementById("user").value;
     var password = document.getElementById("password").value;
-    if (email == "") {
+    if (email === "") {
         alert("Please enter your email");
         return false;
     }
-    else if (password == "") {
+    else if (password === "") {
         alert("Please enter your password");
         return false;
     }
     else {
+        acess_login(email, password)
+    }
+}
+
+
+async function acess_login (email, password) {
+        
+    let options = {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Accept': 'application/json'
+        },
+        body: 'grant_type=&username=' + email + '&password=' + password + '&scope=&client_id=&client_secret='
+    };
+
+    let ret = await fetch(`http://127.0.0.1:8000/token`, options);
+
+
+    if (ret.status === 200)
+    {
+        let data = await ret.json();
+        localStorage.setItem("token", data);
         document.location = "./pages/home.html";
-        return true;
+    }
+    else
+    {
+        alert("Invalid email or password");
     }
 }
